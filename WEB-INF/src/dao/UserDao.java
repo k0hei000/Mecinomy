e0fe2b77c2112d221821596dc32f60bc733e90b8
mizuki_kojima <mizuki_kojima@r.recruit.co.jp>
2016-05-05 02:12:43 +0900

    aaaa

--- WEB-INF/src/dao/UserDao.java
+++ WEB-INF/src/dao/UserDao.java
@@ -40,7 +40,7 @@ public class UserDao extends DbConnectDao {
 		UserBean user = null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
-		String sql = "SELECT user_id,password FROM usertable " +
+		String sql = "SELECT user_id, password FROM usertable " +
 				"where usertable.is_delete < 1 and usertable.user_id = ? and usertable.password = ?";
 		try{
 			pstmt = con.prepareStatement(sql);
