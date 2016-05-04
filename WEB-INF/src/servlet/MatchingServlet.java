fd2b8c6130df22b417a01a9d047a338f82270c30
oroku <oroku@r.recruit.co.jp>
2016-05-05 00:56:16 +0900

    Servlet修正

--- WEB-INF/src/servlet/MatchingServlet.java
+++ WEB-INF/src/servlet/MatchingServlet.java
@@ -25,36 +25,45 @@ public class MatchingServlet extends HttpServlet {
 	private static final long serialVersionUID = -6945929109627628817L;
 	protected void doPost(HttpServletRequest request , HttpServletResponse response)
 			throws ServletException,IOException {
-		ServletContext sc = null;
-		//エラーメッセージの一時保存用変数
-		String message = null;
 		//デフォルトの転送先
 		String destination = null;
 		//エラーメッセージ処理クラスのインスタンス化
 		ArrayList<String> error = new ArrayList<String>();
 		//セッションの取得（なければ作成）
 		HttpSession session = request.getSession(true);
-		String purpose = (String) session.getAttribute("purpose");
-		String timeStart = (String) session.getAttribute("timeStart");
-		String timeEnd = (String) session.getAttribute("timeEnd");
-		String userId = (String) session.getAttribute("userId");
-		PostBean post = new PostBean(purpose,timeStart,timeEnd,userId);
-//		GroupBean group = new GroupBean();
-		//matchingのロジック
-		MatchingLogic logic = new MatchingLogic();
-		MatchingBean match = null;
-		try {
-			match = logic.matchingCheck(post);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
-		if(match == null){
-			destination = "jsp/match/wait.jsp";
+		String purpose = request.getParameter("purpose");
+		String timeStart = request.getParameter("timeStart");
+		String timeEnd = request.getParameter("timeEnd");
+		String userId = request.getParameter("userId");
+		if (userId == null || purpose == null) {
+			error.add("null user or purpose!");
+			System.out.println("error!");
+			session.setAttribute("errormessage",error);
 		} else {
-			destination = "jsp/match/match.jsp";
+			PostBean post = new PostBean(purpose,timeStart,timeEnd,userId);
+//			GroupBean group = new GroupBean();
+			//matchingのロジック
+			MatchingLogic logic = new MatchingLogic();
+			MatchingBean match = null;
+			try {
+				match = logic.matchingCheck(post);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+			if(match == null){
+				destination = "/WEB-INF/jsp/match/wait.jsp";
+			} else {
+				session.setAttribute("match", match);
+				destination = "/WEB-INF/jsp/match/match.jsp";
+			}
 		}
+		if(!error.isEmpty()){
+			destination = "/WEB-INF/jsp/post.jsp";
+		}
+		//ServletContextオブジェクトを取得
+		ServletContext sc = this.getServletContext();
 		//RequestDispatcherオブジェクトを取得
 		RequestDispatcher rd = sc.getRequestDispatcher(destination);
 		//forwardメソッドで、処理をreceive.jspに転送
